e88556449e56a44841c04ea4863e87b233f0a19f
chamaraf <developandtestanddeploy@gmail.com>
2023-04-17 20:21:49 +0100

    webhooks

--- src/test/java/org/selenium/pom/utils/Dummy.java
+++ src/test/java/org/selenium/pom/utils/Dummy.java
@@ -3,5 +3,6 @@ package org.selenium.pom.utils;
 public class Dummy{
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
     }
 }
